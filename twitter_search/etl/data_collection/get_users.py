1a7cda07db686c74387cb45aac2efa8f2f6ca395
Federico Domínguez Molina <fd.molina@outlook.com>
2024-03-16 23:42:01 -0500

    Completing get users

--- twitter_search/etl/data_collection/get_users.py
+++ twitter_search/etl/data_collection/get_users.py
@@ -1,7 +1,9 @@
 import time
 from pathlib import Path
+from datetime import datetime
 
-from twitter_search.config_utils import constants, util
+from twitter_search.config_utils import util
+from twitter_search.config_utils.constants import MAX_RESULTS
 
 
 class UserParser:
@@ -9,60 +11,61 @@ class UserParser:
     This class is in charge of parsing all of the twitter users
     """
 
-    def __init__(self):
-        pass
+    MAX_RESULTS = MAX_RESULTS
 
+    def __init__(self, x, location):
+        self.x = x
+        self.location = location
 
-def get_users_fromlists(client, lists_data, output_file, k=None):
-    unique = set()
-    count = 0
-    if k is None:
-        k = len(lists_data) - 1
-    for item in lists_data[:k]:
+    def get_users_fromlists(self, client, lists_data, output_file, k=None):
+        unique = set()
+        count = 0
+        if k is None:
+            k = len(lists_data) - 1
+        for item in lists_data[:k]:
+            try:
+                list_id = item
+                print("list_id", list_id)
+                if list_id is not None and list_id not in unique:
+                    unique.add(list_id)
+                    users = client.get_list_members(
+                        id=list_id, max_results=50, user_fields=util.USER_FIELDS
+                    )
+                    user_dicts = util.user_dictmaker(users.data)
+                    util.json_maker(output_file, user_dicts)
+            except Exception as e:
+                print(f"Error fetching users for list {item}: {e}")
+                continue
+            count += 1
+            if count > 24:
+                print("You have to wait for 15 mins")
+                i = 1
+                while i <= 3:
+                    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    time.sleep(300)
+                    print(f"{current_time} - {i * 5} minutes done out of 15")
+                    i += 1
+                count = 0
+            time.sleep(1)
+            # TODO
+            # client = util.client_creator()
+
+    def get_users(self):
         try:
-            list_id = item
-            print("list_id", list_id)
-            if list_id is not None and list_id not in unique:
-                unique.add(list_id)
-                users = client.get_list_members(
-                    id=list_id, max_results=50, user_fields=util.USER_FIELDS
-                )
-                user_dicts = util.user_dictmaker(users.data)
-                util.json_maker(output_file, user_dicts)
+            dir = Path(__file__).parent.parent.parent / "data/raw_data"
+            input_file = dir / f"{self.location}_lists.json"
+            output_file = dir / f"{self.location}_totalusers.json"
+            print("till here")
+            lists_data = util.load_json(input_file)
+            print(lists_data[:10], "here you go")
+            client = util.client_creator()
+            print("client created")
+            isolated_lists = util.flatten_and_remove_empty(lists_data)
+            print(len(isolated_lists))
+            filtered_lists = util.list_filter_keywords(isolated_lists, self.location)
+            print(len(filtered_lists))
+            self.get_users_fromlists(client, filtered_lists, output_file)
+
+            return self.x
         except Exception as e:
-            print(f"Error fetching users for list {item}: {e}")
-            continue
-        count += 1
-        if count > 24:
-            print("You have to wait for 15 mins")
-            i = 1
-            while i <= 3:
-                current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                time.sleep(300)
-                print(f"{current_time} - {i * 5} minutes done out of 15")
-                i += 1
-            count = 0
-        time.sleep(1)
-        # TODO
-        # client = util.client_creator()
-
-
-def get_users(x, location):
-    try:
-        dir = Path(__file__).parent.parent.parent / "data/raw_data"
-        input_file = dir / f"{location}_lists.json"
-        output_file = dir / f"{location}_totalusers.json"
-        print("till here")
-        lists_data = util.load_json(input_file)
-        print(lists_data[:10], "here you go")
-        client = util.client_creator()
-        print("client created")
-        isolated_lists = util.flatten_and_remove_empty(lists_data)
-        print(len(isolated_lists))
-        filtered_lists = util.list_filter_keywords(isolated_lists, location)
-        print(len(filtered_lists))
-        get_users_fromlists(client, filtered_lists, output_file)
-
-        return x
-    except Exception as e:
-        print(f"An error occurred: {e}")
+            print(f"An error occurred: {e}")
