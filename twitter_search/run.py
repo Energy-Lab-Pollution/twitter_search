aac71d87a3ff5f08c27ef948f4f8e6271c2f7c22
Federico Domínguez Molina <fd.molina@outlook.com>
2024-03-17 23:51:15 -0500

    Style changes

--- twitter_search/run.py
+++ twitter_search/run.py
@@ -2,6 +2,7 @@ from twitter_search.etl import run_search_twitter
 import sys
 from argparse import ArgumentParser
 
+
 def build_query(location):
 
     return f"(air pollution {location} OR {location} air OR {location} \
@@ -9,21 +10,29 @@ def build_query(location):
         {location} asthma OR {location} polluted OR pollution control board) \
         (#pollution OR #environment OR #cleanair OR #airquality) -is:retweet"
 
+
 def lets_getit(location):
-        print("here")
-        # Build the query based on args.location
-        query = build_query(location)
-        print(query)
-        run_search_twitter.run_search_twitter(query,location)
+    print("here")
+    # Build the query based on args.location
+    query = build_query(location)
+    print(query)
+    run_search_twitter.run_search_twitter(query, location)
+
 
 def main():
     try:
-        parser = ArgumentParser(description = "Get users from Twitter \
-                                based on location and algorithm.")
-        parser.add_argument("--location", type=str, help="Specify the\
-                             location (city) for Twitter user search.")
+        parser = ArgumentParser(
+            description="Get users from Twitter \
+                                based on location and algorithm."
+        )
+        parser.add_argument(
+            "--location",
+            type=str,
+            help="Specify the\
+                             location (city) for Twitter user search.",
+        )
         # parser.add_argument("--algorithm", type=int, choices=[1, 2], \
-        #help="Specify the algorithm (1 or 2).")
+        # help="Specify the algorithm (1 or 2).")
         args = parser.parse_args()
 
         if not args.location:
@@ -34,6 +43,3 @@ def main():
             lets_getit(location)
     except:
         print("error")
-
-
-
