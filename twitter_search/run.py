b64a3cfeaa239d787a206c39943f7867b4310a16
Federico Domínguez Molina <fd.molina@outlook.com>
2024-03-19 11:12:29 -0500

    Removing unnecessary import

--- twitter_search/run.py
+++ twitter_search/run.py
@@ -3,7 +3,6 @@ Main function to run the Twitter search and data collection process.
 """
 
 from twitter_search.etl import run_search_twitter
-import sys
 from argparse import ArgumentParser
 
 
@@ -13,14 +12,6 @@ def build_query(location):
         pollution OR {location} public health OR bad air {location} OR \
         {location} asthma OR {location} polluted OR pollution control board) \
         (#pollution OR #environment OR #cleanair OR #airquality) -is:retweet"
-
-
-def lets_getit(location):
-    print("Building query...")
-    # Build the query based on args.location
-    query = build_query(location)
-    print(query)
-    run_search_twitter.run_search_twitter(query, location)
 
 
 def main():
@@ -40,6 +31,11 @@ def main():
     args = parser.parse_args()
     try:
         location = args.location
-        lets_getit(location)
+        print("Building query...")
+        # Build the query based on args.location
+        query = build_query(location)
+        print(query)
+        run_search_twitter.run_search_twitter(query, location)
+
     except Exception as e:
         print(f"Error running process: {e}")
