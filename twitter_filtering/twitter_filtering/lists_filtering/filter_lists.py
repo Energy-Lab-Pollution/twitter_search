2d2a75e8651058425ad1ba6a726c7bf045532856
Federico Domínguez Molina <fd.molina@outlook.com>
2024-04-03 00:14:45 -0500

    Modularized code into class

--- twitter_filtering/twitter_filtering/lists_filtering/filter_lists.py
+++ twitter_filtering/twitter_filtering/lists_filtering/filter_lists.py
@@ -16,45 +16,61 @@ from twitter_filtering.utils.constants import PATH, LISTS_KEYWORDS, COLS_TO_KEEP
 # Functions and pipeline
 
 
-def read_json(file_path):
-    """
-    Reads JSON file and returns a dictionary
+class ListReader:
 
-    Args:
-        file_path (str): Path to the JSON file
-    Returns:
-        data (dict): Dictionary with the JSON data
-    """
-    with open(file_path, "r") as file:
-        data = json.load(file)
-    return data
+    PATH = PATH
+    LISTS_KEYWORDS = LISTS_KEYWORDS
+    COLS_TO_KEEP = COLS_TO_KEEP
 
+    def __init__(self) -> None:
+        pass
 
-def create_df(twitter_lists):
-    """
-    Creates a DataFrame from the JSON data
+    def read_json(self):
+        """
+        Reads JSON file and returns a dictionary
 
-    Args:
-        lists (list): List of dictionaries with the JSON data
-    Returns:
-        df (pd.DataFrame): DataFrame with the JSON data
-    """
+        Args:
+            file_path (str): Path to the JSON file
+        Returns:
+            data (dict): Dictionary with the JSON data
+        """
+        with open(self.PATH, "r") as file:
+            data = json.load(file)
+        self.twitter_lists = data
 
-    lists_df = pd.DataFrame([])
+    def parse_into_df(self):
+        """
+        Creates a DataFrame from the JSON data
 
-    for twitter_list in twitter_lists:
-        if not twitter_list:
-            # remove entry if empty
-            continue
+        Args:
+            lists (list): List of dictionaries with the JSON data
+        Returns:
+            df (pd.DataFrame): DataFrame with the JSON data
+        """
 
-        else:
-            # create a dataframe from the list
-            list_df = pd.DataFrame(twitter_list)
-            lists_df = pd.concat([lists_df, list_df], ignore_index=True)
+        self.lists_df = pd.DataFrame([])
 
-    lists_df = lists_df.loc[:, COLS_TO_KEEP].copy()
+        for twitter_list in self.twitter_lists:
+            if not twitter_list:
+                # remove entry if empty
+                continue
 
-    return lists_df
+            else:
+                # create a dataframe from the list
+                list_df = pd.DataFrame(twitter_list)
+                self.lists_df = pd.concat([self.lists_df, list_df], ignore_index=True)
+
+        self.lists_df = self.lists_df.loc[:, COLS_TO_KEEP].copy()
+
+    def create_df(self):
+        """
+        Performs the complete pipeline to get dataframe of lists
+        """
+        self.read_json(f"{PATH}/Mumbai_lists.json")
+        self.parse_into_df()
+        self.lists_df.drop_duplicates(subset=["list_id"], inplace=True)
+
+        return self.lists_df
 
 
 class ListFilter:
@@ -116,9 +132,13 @@ class ListFilter:
 
 if __name__ == "__main__":
 
-    twitter_lists = read_json(f"{PATH}/Mumbai_lists.json")
-    lists_df = create_df(twitter_lists)
-    lists_df.drop_duplicates(subset=["list_id"], inplace=True)
+    print("Reading JSON to create Lists dataframe...")
+    print(f"Reading {PATH}")
+    list_reader = ListReader()
+    lists_df = list_reader.create_df()
+
+    print("Filtering dataframe for relevant lists")
     list_filter = ListFilter(lists_df)
     lists_df["relevant"] = lists_df.apply(list_filter.is_relevant, axis=1)
-    relevant_lists = lists_df.loc[lists_df["relevant"] == True, :]
+    relevant_lists = lists_df.loc[lists_df["relevant"].isin([True]), :]
+    print("Done!")
