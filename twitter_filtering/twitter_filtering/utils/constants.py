3a3af9204fa4a369cfd4643ef0c3504096bb6169
Federico Domínguez Molina <fd.molina@outlook.com>
2024-04-03 18:44:43 -0500

    Remove repeated constantss files

--- twitter_filtering/twitter_filtering/utils/constants.py
+++
@@ -1,8 +0,0 @@
-"""
-Constants used throughout the project
-"""
-
-PATH = "C:/Users/fdmol/Desktop/Energy-Lab/twitter_search/twitter_search/data/raw_data"
-COLS_TO_KEEP = ["user_id", "list_id", "name", "description"]
-
-LISTS_KEYWORDS = ["air", "pollution", "earth", "climate", "smog"]
