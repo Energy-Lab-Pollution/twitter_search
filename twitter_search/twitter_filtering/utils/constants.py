20081896c288960ab9ac40d6e1e26ecddf010f9f
Federico Domínguez Molina <fd.molina@outlook.com>
2024-04-06 21:21:13 -0500

    Getting rid of absolute paths

--- twitter_search/twitter_filtering/utils/constants.py
+++ twitter_search/twitter_filtering/utils/constants.py
@@ -1,10 +1,20 @@
+"""
+Script with constants for the list filtering process
+"""
+
+# Global imports
+
+from pathlib import Path
+
+
 # Lists_filtering constants
 
-RAW_DATA_PATH = (
-    "C:/Users/fdmol/Desktop/Energy-Lab/twitter_search/twitter_search/data/raw_data"
-)
-CLEAN_DATA_PATH = (
-    "C:/Users/fdmol/Desktop/Energy-Lab/twitter_search/twitter_search/data/cleaned_data"
-)
+script_path = Path(__file__).resolve()
+project_root = script_path.parents[3]
+
+# Construct the path to the cleaned_data directory
+RAW_DATA_PATH = project_root / "data" / "raw_data"
+CLEAN_DATA_PATH = project_root / "data" / "cleaned_data"
+
 COLS_TO_KEEP = ["user_id", "list_id", "name", "description"]
 LISTS_KEYWORDS = ["air", "pollution", "earth", "climate", "smog"]
