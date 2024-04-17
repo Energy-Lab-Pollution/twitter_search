9e8fb53c8bb5423a3603aedd8d3b61e399c9ce94
Federico Domínguez Molina <fd.molina@outlook.com>
2024-04-17 00:27:21 -0500

    Doing some refactoring on run_search_twitter

--- twitter_search/etl/run_search_twitter.py
+++ twitter_search/etl/run_search_twitter.py
@@ -39,8 +39,10 @@ def run_search_twitter(query, location):
 
         output_file_filter = dir / f"{location}_users_filtered_{count}.json"
         input_file_lists = output_file_filter
+
         output_file_lists = dir / f"{location}_lists_{count}.json"
         input_file_total = output_file_lists
+
         output_file_total = dir / f"{location}_totalusers_{count}.json"
 
         print(f"Iteration {count}:")
@@ -48,22 +50,28 @@ def run_search_twitter(query, location):
         if count == 1:
             # Perform search only in the first iteration
             print("Searching for Twitter users...")
-            search_twitter_users(location, query, output_file_search)
+
+            user_searcher = UserSearcher(location, output_file_search, query)
+            user_searcher.run_search_all()
 
         # Filter users based on location
         print("Filtering Twitter users based on location...")
-        filter_twitter_users(location, input_file_filter, output_file_filter)
+
+        user_filter = UserFilter(location, input_file_filter, output_file_filter)
+        user_filter.run_filtering()
 
         # Retrieve lists associated with filtered users
         print("Retrieving lists associated with filtered users...")
-        retrieve_lists(location, input_file_lists, output_file_lists)
+        list_getter = ListGetter(location, input_file_lists, output_file_lists)
+        list_getter.get_lists()
 
         # Filter lists
         filter_twitter_lists(location, input_file_filter, output_file_filter)
 
         # Retrieve user data from the retrieved lists
         print("Retrieving user data from lists...")
-        retrieve_user_data(location, input_file_total, output_file_total)
+        user_getter = UserGetter(location, input_file_total, output_file_total)
+        user_getter.get_users()
 
         # Increment count for the next iteration
         count += 1
@@ -75,16 +83,6 @@ def run_search_twitter(query, location):
             break
 
     return "Data collection and cleaning process completed."
-
-
-def search_twitter_users(location, query, output_file_search):
-    user_searcher = UserSearcher(location, output_file_search, query)
-    user_searcher.run_search_all()
-
-
-def filter_twitter_users(location, input_file_filter, output_file_filter):
-    user_filter = UserFilter(location, input_file_filter, output_file_filter)
-    user_filter.run_filtering()
 
 
 def filter_twitter_lists(location, input_file_filter, output_file_filter):
@@ -99,16 +97,6 @@ def filter_twitter_lists(location, input_file_filter, output_file_filter):
 
     new_filename = input_file_filter.replace(".json", "_filtered.json")
     relevant_lists.to_json(f"{new_filename}", orient="records")
-
-
-def retrieve_lists(location, input_file_lists, output_file_lists):
-    list_getter = ListGetter(location, input_file_lists, output_file_lists)
-    list_getter.get_lists()
-
-
-def retrieve_user_data(location, input_file_total, output_file_total):
-    user_getter = UserGetter(location, input_file_total, output_file_total)
-    user_getter.get_users()
 
 
 def additional_iterations_needed(count):
