df3e6b83d5b3371c046484e202d9504ec0a79e62
Federico Domínguez Molina <fd.molina@outlook.com>
2024-05-02 12:54:28 -0500

    Creating class for generating csv files

--- twitter_search/etl/create_csvs.py
+++ twitter_search/etl/create_csvs.py
@@ -11,90 +11,93 @@ from pathlib import Path
 
 # Lists_filtering constants
 
+
 script_path = Path(__file__).resolve()
 project_root = script_path.parents[1]
 
-# Construct the path to the cleaned_data directory
-RAW_DATA_PATH = project_root / "data" / "raw_data"
-CLEAN_DATA_PATH = project_root / "data" / "cleaned_data"
 
+class CSVConverter:
+    # Construct the path to the cleaned_data directory
+    RAW_DATA_PATH = project_root / "data" / "raw_data"
+    CLEAN_DATA_PATH = project_root / "data" / "cleaned_data"
 
-def filter_json_files(json_files, location):
-    """
-    Filter the JSON files based on the location.
+    def __init__(self) -> None:
+        # See which JSON files are available
+        self.json_files = os.listdir(self.RAW_DATA_PATH)
 
-    Args:
-        json_files (list): The list of JSON files.
-        location (str): The location to filter on.
+    def filter_json_files(json_files, location):
+        """
+        Filter the JSON files based on the location.
 
-    Returns:
-        list: The filtered JSON files.
-    """
-    # Filter the JSON files based on the location
-    filtered_files = [
-        file for file in json_files if location.lower() in file.lower()
-    ]
+        Args:
+            json_files (list): The list of JSON files.
+            location (str): The location to filter on.
 
-    return filtered_files
+        Returns:
+            list: The filtered JSON files.
+        """
+        # Filter the JSON files based on the location
+        filtered_files = [
+            file for file in json_files if location.lower() in file.lower()
+        ]
 
+        return filtered_files
 
-def convert_to_df(input_file):
-    """
-    Convert JSON files into CSV files.
+    def convert_to_df(self, input_file):
+        """
+        Convert JSON files into CSV files.
 
-    Args:
-        input_file (str): The input JSON file.
-        output_file (str): The output CSV file.
+        Args:
+            input_file (str): The input JSON file.
+            output_file (str): The output CSV file.
 
-    Returns:
-        None
-    """
-    # Load the JSON file
-    with open(input_file, "r") as json_file:
-        data = json.load(json_file)
+        Returns:
+            None
+        """
+        # Load the JSON file
+        with open(input_file, "r") as json_file:
+            data = json.load(json_file)
 
-    # Convert the JSON data into a DataFrame
+        # Convert the JSON data into a DataFrame
 
-    # If nested list
+        # If nested list
 
-    if isinstance(data[0], list):
-        df = pd.DataFrame(data[0])
+        if isinstance(data[0], list):
+            df = pd.DataFrame(data[0])
 
-    else:
-        df = pd.DataFrame(data)
+        else:
+            df = pd.DataFrame(data)
 
-    # # Save the DataFrame as a CSV file
-    # df.to_csv(output_file, index=False)
+        # # Save the DataFrame as a CSV file
+        # df.to_csv(output_file, index=False)
 
-    return df
+        return df
 
+    def concat_dataframes(self, files_list):
+        """
+        Reads the JSON files, creates a dataframe
+        for each file and concatenates all the dataframes.
 
-def concat_dataframes(files_list):
-    """
-    Reads the JSON files, creates a dataframe
-    for each file and concatenates all the dataframes.
+        Args:
+            files_list (list): List of JSON files.
 
-    Args:
-        files_list (list): List of JSON files.
+        Returns:
+            DataFrame: The concatenated DataFrame.
+        """
 
-    Returns:
-        DataFrame: The concatenated DataFrame.
-    """
+        df = pd.DataFrame()
 
-    df = pd.DataFrame()
+        for file in files_list:
+            input_file = self.RAW_DATA_PATH / file
+            print(f"Converting {input_file} to CSV")
+            df = pd.concat(
+                [df, self.convert_to_df(input_file)], ignore_index=True
+            )
 
-    for file in files_list:
-        input_file = RAW_DATA_PATH / file
-        print(f"Converting {input_file} to CSV")
-        df = pd.concat([df, convert_to_df(input_file)], ignore_index=True)
-
-    return df
+        return df
 
 
 if __name__ == "__main__":
-
-    # See which JSON files are available
-    json_files = os.listdir(RAW_DATA_PATH)
 
     # Filter the JSON files based on the location
     location = "Bangalore"
