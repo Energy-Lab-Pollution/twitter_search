977be8aa2a4a82ffdd4035d138bf3efc5a01c4f7
Federico Domínguez Molina <fd.molina@outlook.com>
2024-05-08 21:14:53 -0500

    .

--- twitter_search/twitter_filtering/users_filtering/filter_users.py
+++ twitter_search/twitter_filtering/users_filtering/filter_users.py
@@ -39,8 +39,12 @@ class UserFilter:
 
         try:
             self.users_list = util.load_json(self.input_file)
-            self.total_user_dict = util.flatten_and_remove_empty(self.users_list)
-            self.total_user_dict = util.remove_duplicate_records(self.total_user_dict)
+            self.total_user_dict = util.flatten_and_remove_empty(
+                self.users_list
+            )
+            self.total_user_dict = util.remove_duplicate_records(
+                self.total_user_dict
+            )
             print("users look like this:", self.total_user_dict[0])
         except Exception as e:
             print(f"Error loading data: {e}")
@@ -60,9 +64,17 @@ class UserFilter:
             user["token"] = " ".join(
                 [
                     user["username"],
-                    (user["description"] if user["description"] is not None else ""),
+                    (
+                        user["description"]
+                        if user["description"] is not None
+                        else ""
+                    ),
                     user["location"] if user["location"] is not None else "",
-                    (" ".join(user["tweets"]) if user["tweets"] is not None else ""),
+                    (
+                        " ".join(user["tweets"])
+                        if user["tweets"] is not None
+                        else ""
+                    ),
                 ]
             )
             try:
@@ -127,9 +139,13 @@ class UserFilter:
                         print(f"Error determining subnational location: {e}")
                         subnational = None
                     print(subnational, "subnational")
-                    desired_locations = constants.STATE_CAPITALS.get(self.location, [])
+                    desired_locations = constants.STATE_CAPITALS.get(
+                        self.location, []
+                    )
                     print("desired locations", desired_locations)
-                    user["location_relevance"] = subnational in desired_locations
+                    user["location_relevance"] = (
+                        subnational in desired_locations
+                    )
                 else:
                     user["location_relevance"] = False
 
@@ -140,8 +156,8 @@ class UserFilter:
 
         self.filtered_user = []
         for user in self.total_user_dict:
-            #if user["content_is_relevant"] is True:
-                self.filtered_user.append(user)
+            # if user["content_is_relevant"] is True:
+            self.filtered_user.append(user)
 
         print(f"Filtered {len(self.filtered_user)} relevant users")
 
@@ -170,7 +186,7 @@ class UserFilter:
             self.determine_location_relevance()
             print(f"relevant users for {self.location} tagged step 4 done \n")
             self.remove_users()
-            #print("non-relevant users removed, step 5 completed \n")
+            # print("non-relevant users removed, step 5 completed \n")
             self.store_users()
             print("Filtered users stored successfully.")
 
