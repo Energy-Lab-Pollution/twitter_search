55fc3072481720dfafb249a7b712c4757bb5c068
Federico Domínguez Molina <fd.molina@outlook.com>
2024-07-16 15:27:05 -0500

    Save analysis

--- twitter_search/etl/data_analysis/user_analysis.py
+++ twitter_search/etl/data_analysis/user_analysis.py
@@ -39,15 +39,33 @@ def get_users_per_city(all_users_df):
     """
     user_cities = all_users_df.groupby(by=["search_location"]).count()
     user_cities.reset_index(drop=False, inplace=True)
-    user_cities.rename(columns={"user_id": "count"}, inplace=True)
-    user_cities = user_cities.loc[:, ["search_location", "count"]]
+    user_cities.rename(columns={"user_id": "total_count"}, inplace=True)
+    user_cities = user_cities.loc[:, ["search_location", "total_count"]]
 
     return user_cities
+
+
+def get_percentages(user_types, user_cities):
+    """
+    Gets percentages of user type per city
+    """
+    final_df = pd.merge(
+        user_types, user_cities, how="left", on="search_location"
+    )
+
+    final_df.loc[:, "percentage"] = (
+        final_df.loc[:, "count"] / final_df.loc[:, "total_count"]
+    )
+
+    final_df.to_csv(f"{CLEAN_DATA_PATH}/analysis.csv", index=False)
+
+    return final_df
 
 
 if __name__ == "__main__":
 
     user_types = get_user_types_by_city(all_users_df)
     user_cities = get_users_per_city(all_users_df)
+    final_df = get_percentages(user_types, user_cities)
 
-    print(user_cities)
+    print(final_df)
