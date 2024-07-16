85478df67e0051205e257131b8ef4782105394ea
Federico Domínguez Molina <fd.molina@outlook.com>
2024-07-16 14:12:37 -0500

    Adding function to get total users per city

--- twitter_search/etl/data_analysis/user_analysis.py
+++ twitter_search/etl/data_analysis/user_analysis.py
@@ -14,12 +14,11 @@ all_users_df = pd.read_csv(
     f"{CLEAN_DATA_PATH}/all_users.csv", encoding="utf-8-sig"
 )
 
+
 # Analyze user types
-
-
 def get_user_types_by_city(all_users_df):
     """
-    Gets user types by city
+    Gets total number of users, distinguished by type and city
     """
     user_types = all_users_df.groupby(
         by=["search_location", "search_account_type"]
@@ -34,5 +33,21 @@ def get_user_types_by_city(all_users_df):
 
 
 # Get totals by city
-user_cities = all_users_df.groupby(by=["search_location"]).count()
-user_cities.reset_index(drop=False, inplace=True)
+def get_users_per_city(all_users_df):
+    """
+    Get total number of users per city
+    """
+    user_cities = all_users_df.groupby(by=["search_location"]).count()
+    user_cities.reset_index(drop=False, inplace=True)
+    user_cities.rename(columns={"user_id": "count"}, inplace=True)
+    user_cities = user_cities.loc[:, ["search_location", "count"]]
+
+    return user_cities
+
+
+if __name__ == "__main__":
+
+    user_types = get_user_types_by_city(all_users_df)
+    user_cities = get_users_per_city(all_users_df)
+
+    print(user_cities)
