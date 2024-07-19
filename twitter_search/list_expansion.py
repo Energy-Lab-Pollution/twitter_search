c85b644861c951c8995e6f0d4fab8187359b05c4
Federico Domínguez Molina <fd.molina@outlook.com>
2024-07-19 17:23:54 -0500

    Removing unnecessary imports

--- twitter_search/list_expansion.py
+++ twitter_search/list_expansion.py
@@ -4,10 +4,10 @@ Main function to run the List Expansion collection process.
 
 from argparse import ArgumentParser
 
-from config_utils.cities import CITIES
+# from config_utils.cities import CITIES
 
 # Local imports
-from etl.data_cleaning.csv_converter import CSVConverter
+# from etl.data_cleaning.csv_converter import CSVConverter
 from etl.lists_handler import ListsHandler
 
 
@@ -44,29 +44,21 @@ def main():
 
     print("Building query...")
 
-    twitter_data_handler = ListsHandler(location, account_type)
-
-    if args.num_iterations:
-        print(
-            f"""Running search with {args.num_iterations} iterations for {location}
-            and {account_type} account type"""
-        )
-        num_iterations = args.num_iterations
-        twitter_data_handler.num_iterations = num_iterations
+    lists_handler = ListsHandler(location, account_type)
 
     if args.account_type == "all":
         if args.location == "all":
-            twitter_data_handler.run_all_locations_accounts()
-            for city in CITIES:
-                csv_converter = CSVConverter(city)
-                csv_converter.run()
+            lists_handler.list_expansion_all_locations()
+            # for city in CITIES:
+            #     csv_converter = CSVConverter(city)
+            #     csv_converter.run()
 
         else:
-            twitter_data_handler.run_all_account_types()
-            csv_converter = CSVConverter(args.location)
-            csv_converter.run()
+            lists_handler.list_expansion_all_account_types()
+            # csv_converter = CSVConverter(args.location)
+            # csv_converter.run()
     else:
-        twitter_data_handler.run()
+        lists_handler.run()
 
 
 if __name__ == "__main__":
