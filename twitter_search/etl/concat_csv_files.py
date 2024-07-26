6438bfb426aa7ba92ad845e43313746b7dd9cb5c
Federico Domínguez Molina <fd.molina@outlook.com>
2024-07-26 14:43:14 -0500

    Creating class for concating csv files

--- twitter_search/etl/concat_csv_files.py
+++ twitter_search/etl/concat_csv_files.py
@@ -8,34 +8,39 @@ from pathlib import Path
 import pandas as pd
 
 
-script_path = Path(__file__).resolve()
-project_root = script_path.parents[1]
+class CSVConcat:
 
-CLEAN_DATA_PATH = project_root / "data" / "cleaned_data"
+    def __init__(self):
 
+        self.script_path = Path(__file__).resolve()
+        self.project_root = self.script_path.parents[1]
+        self.CLEAN_DATA_PATH = self.project_root / "data" / "cleaned_data"
 
-def concat_files():
-    """
-    Concatenates all of the csv files
+    def concat_files(self):
+        """
+        Concatenates all of the csv files
 
-    Reads all of the available files and concatenates them
-    """
+        Reads all of the available files and concatenates them
+        """
 
-    csv_files = os.listdir(CLEAN_DATA_PATH)
-    csv_files = [file for file in csv_files if "user_data" in file]
+        csv_files = os.listdir(self.CLEAN_DATA_PATH)
+        csv_files = [file for file in csv_files if "user_data" in file]
 
-    all_users = pd.DataFrame()
+        all_users = pd.DataFrame()
 
-    for csv_file in csv_files:
-        df = pd.read_csv(f"{CLEAN_DATA_PATH}/{csv_file}")
-        all_users = pd.concat([all_users, df], ignore_index=True)
+        for csv_file in csv_files:
+            df = pd.read_csv(f"{self.CLEAN_DATA_PATH}/{csv_file}")
+            all_users = pd.concat([all_users, df], ignore_index=True)
 
-    all_users.to_csv(
-        f"{CLEAN_DATA_PATH}/all_users.csv", index=False, encoding="utf-8-sig"
-    )
+        all_users.to_csv(
+            f"{self.CLEAN_DATA_PATH}/all_users.csv",
+            index=False,
+            encoding="utf-8-sig",
+        )
 
-    print("Successfully concatenated all csv files")
+        print("Successfully concatenated all csv files")
 
 
 if __name__ == "__main__":
-    concat_files()
+    csv_concat = CSVConcat()
+    # concat_files()
