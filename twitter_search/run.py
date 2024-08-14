1044d04271683862b292f19b8b72eb834e9a4e23
Federico Domínguez Molina <fd.molina@outlook.com>
2024-08-14 14:40:49 -0500

    Adding argparse to check if media accounts want to be skipped or not

--- twitter_search/run.py
+++ twitter_search/run.py
@@ -5,6 +5,7 @@ Main function to run the Twitter search and data collection process.
 from argparse import ArgumentParser
 
 from config_utils.cities import CITIES
+from config_utils.util import strtobool
 
 # Local imports
 from etl.data_cleaning.csv_converter import CSVConverter
@@ -37,10 +38,12 @@ def main():
             "all",
         ],
     )
+
     parser.add_argument(
-        "--num_iterations",
-        type=int,
-        help="Specify the number of iterations to run.",
+        "--skip_media",
+        type=str,
+        help="Specify if the media accounts should be skipped",
+        options=["True", "False"],
     )
 
     args = parser.parse_args()
@@ -50,22 +53,32 @@ def main():
 
     twitter_data_handler = TwitterDataHandler(location, account_type)
 
+    if args.skip_media:
+        skip_media = strtobool(args.skip_media)
+
     if args.num_iterations:
         print(
-            f"""Running search with {args.num_iterations} iterations for {location}
-            and {account_type} account type"""
+            f"""Running search  for {location} and {account_type} account type"""
         )
         num_iterations = args.num_iterations
         twitter_data_handler.num_iterations = num_iterations
 
     if args.account_type == "all":
         if args.location == "all":
-            twitter_data_handler.run_all_locations_accounts()
+            if args.skip_media:
+                twitter_data_handler.run_all_locations_accounts(skip_media)
+            else:
+                twitter_data_handler.run_all_locations_accounts()
+
             for city in CITIES:
                 csv_converter = CSVConverter(city)
                 csv_converter.run()
 
         else:
+            if args.skip_media:
+                twitter_data_handler.run_all_account_types(skip_media)
+            else:
+                twitter_data_handler.run_all_account_types()
             twitter_data_handler.run_all_account_types()
             csv_converter = CSVConverter(args.location)
             csv_converter.run()
