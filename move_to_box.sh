1f8c5a9645c8fc5e8f2a1a2f256774165d1bd38f
Federico Domínguez Molina <fd.molina@outlook.com>
2024-08-19 14:47:11 -0500

    Testing script

--- move_to_box.sh
+++ move_to_box.sh
@@ -1,1 +1,6 @@
 # Run this bash script to move files to box folder
+
+source_dir="C:/Users(fdmol/Desktop/Energy-Lab/twitter_search/twitter_search/data/"
+destination_dir="C:/Users/fdmol/Box/Global RCT/data"
+
+cp -r $source_dir $destination_df
