f0ad6ad0e74e5b85379347c957ee4e6177b81f0a
Federico Domínguez Molina <fd.molina@outlook.com>
2024-08-22 11:21:08 -0500

    Added pilot cities, also fixed bash script to move fies

--- move_to_box.sh
+++ move_to_box.sh
@@ -1,6 +1,12 @@
 # Run this bash script to move files to box folder
 
-source_dir="C:/Users(fdmol/Desktop/Energy-Lab/twitter_search/twitter_search/data/"
-destination_dir="C:/Users/fdmol/Box/Global RCT/data"
+source_dir="C:/Users/fdmol/Desktop/Energy-Lab/twitter_search/twitter_search/data/"
+destination_dir="C:/Users/fdmol/Box/Global-RCT/data"
 
-cp -r $source_dir $destination_df
+echo "Copying user data files..."
+
+if cp -r "$source_dir"/* "$destination_dir"/; then
+  echo "Files copied successfully!"
+else
+  echo "Failed to copy files."
+fi
