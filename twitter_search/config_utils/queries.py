2e763fe4fef0d8470eb57e9ec2293a55af582aa4
Federico Domínguez Molina <fd.molina@outlook.com>
2024-08-26 11:15:44 -0500

    Query adjust

--- twitter_search/config_utils/queries.py
+++ twitter_search/config_utils/queries.py
@@ -1,8 +1,8 @@
 # QUERY DICTIONARY
 QUERIES = {
     "media": """(location)(media OR press OR coverage OR broadcasting
-                    OR alert OR breaking OR journalism OR journalist OR news
-                        OR local OR news OR patrika)
+                OR alert OR breaking OR journalism OR journalist OR news
+                OR local OR news OR patrika)
                         -is:retweet""",
     "organizations": """(NGO location OR organization location
                        OR non-profit location OR location OR location institution OR
@@ -11,18 +11,17 @@ QUERIES = {
     "policymaker": """(member of parliament OR minister OR magistrate OR
                     District magistrate OR IAS OR officer OR cabinet OR mayor
                     OR councillor OR localgovernment OR city official OR MLA OR MP)
-                    (location OR location government
-                    OR location council OR location municipality)
-                    (#MP OR #MLA OR #cabinet OR #minister
+                    (location OR location government  OR location council OR location 
+                    municipality) (#MP OR #MLA OR #cabinet OR #minister
                     OR #secretary OR #IAS OR #IPS) -is:retweet""",
     "politicians": """(politics OR politicians) (location
                     OR location politics OR location government)
                     (#politics OR #politician OR #election) -is:retweet""",
     "researcher": """location ((public heath) OR (environmental research)
                     OR (environmental researcher) OR health OR science OR academic
-                    OR research) (#science OR #research OR #academic)
+                    OR research OR university) (#science OR #research OR #academic)
                     -is:retweet""",
-    "environment": """(air pollution location OR location air
+    "environment": """(air pollution location OR location airper
             OR location pollution OR location public health
             OR bad air location OR location asthma OR location
             polluted OR pollution control board) (#pollution OR #environment
