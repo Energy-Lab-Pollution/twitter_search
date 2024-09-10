b62240781b9e66f750c33164c429541cf2b683dd
Federico Domínguez Molina <fd.molina@outlook.com>
2024-09-10 15:47:02 -0500

    Almost done with s3 upload implementation

--- twitter_search/etl/data_storage/s3_upload.py
+++ twitter_search/etl/data_storage/s3_upload.py
@@ -3,13 +3,18 @@ This script will upload the resulting files to AWS
 """
 
 import logging
+import os
+import time
+from pathlib import Path
 
 # Global imports
 import boto3
 import botocore
 
 # Local imports
-from config_utils.constants import BUCKET_NAME, REGION_NAME
+BUCKET_NAME = "global-rct-users"
+REGION_NAME = "us-west-1"
+FOLDERS = ["raw_data", "clean_data"]
 
 
 # Set logger
@@ -32,3 +37,34 @@ def upload_to_s3(local_filename, s3_filename):
 
     except botocore.exceptions.ClientError:
         logger.info("Upload unsuccessful")
+
+
+def upload_directory(directory_path):
+    """
+    Uploads all the files in a single
+    directory to S3
+    """
+    files = os.listdir(directory_path)
+
+    # Should either be raw_data or clean_data
+    s3_folder = str(directory_path).split("/")[-1]
+
+    if s3_folder not in FOLDERS:
+        raise FileNotFoundError(
+            f"Desired folder is not one of {FOLDERS} - please check"
+        )
+
+    for file in files:
+        upload_to_s3(f"{directory_path}/{file}", f"{s3_folder}/{file}")
+
+    logger.info(f"Done uploading files to S3 folder: {s3_folder}")
+
+
+if __name__ == "__main__":
+
+    raw_dir = Path(__file__).parent.parent.parent / "data/raw_data"
+    clean_dir = Path(__file__).parent.parent.parent / "data/clean_data"
+
+    upload_directory(raw_dir)
+    time.sleep(2.5)
+    upload_directory(clean_dir)
