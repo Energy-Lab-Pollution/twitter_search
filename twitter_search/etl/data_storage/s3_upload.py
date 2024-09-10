4f67c2cc9540bab8e9433e3a2d2ffda65902b50a
Federico Domínguez Molina <fd.molina@outlook.com>
2024-09-10 12:28:19 -0500

    Adding S3 upload function

---
+++ twitter_search/etl/data_storage/s3_upload.py
@@ -0,0 +1,33 @@
+"""
+This script will upload the resulting files to AWS
+"""
+
+# Local imports
+from config_utils.constants import BUCKET_NAME, REGION_NAME
+
+import logging
+
+# Global imports
+import boto3
+import botocore
+
+# Set logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+s3_client = boto3.client("s3", region_name=REGION_NAME)
+
+
+def upload_to_s3(local_filename, s3_filename):
+    """
+    Uploads a given file to S3
+    """
+
+    try:
+        s3_client.upload_file(
+            Filename=local_filename, Bucket=BUCKET_NAME, Key=s3_filename
+        )
+
+    except botocore.exceptions.ClientError:
+        logger.info("Upload unsuccessful")
