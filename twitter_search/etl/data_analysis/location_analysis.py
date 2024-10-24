ace700ccfce48ce4934436d5824bd445911fae29
Federico Domínguez Molina <fd.molina@outlook.com>
2024-10-24 11:42:21 -0500

    Add location analysis

--- twitter_search/etl/data_analysis/location_analysis.py
+++ twitter_search/etl/data_analysis/location_analysis.py
@@ -10,7 +10,7 @@ script_path = Path(__file__).resolve()
 project_root = script_path.parents[2]
 CLEAN_DATA_PATH = project_root / "data" / "cleaned_data"
 
-# Read both files
+# Read both to create a single file
 
 default_users = pd.read_csv(f"{CLEAN_DATA_PATH}/all_distinct_users.csv",
                             encoding='utf-8-sig')
