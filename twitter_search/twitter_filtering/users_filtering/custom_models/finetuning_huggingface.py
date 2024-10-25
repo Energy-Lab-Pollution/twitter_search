66b3f668dfb40c85aca274c1386fa9b25bec03d1
Federico Domínguez Molina <fd.molina@outlook.com>
2024-10-25 15:28:05 -0500

    Linting files

--- twitter_search/twitter_filtering/users_filtering/custom_models/finetuning_huggingface.py
+++ twitter_search/twitter_filtering/users_filtering/custom_models/finetuning_huggingface.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import pandas as pd
 from transformers import BartTokenizer
 
+
 script_path = Path(__file__).resolve()
 project_root = script_path.parents[3]
 CLEAN_DATA_PATH = project_root / "data" / "labeled_data"
@@ -52,17 +53,18 @@ def labeled_data_to_csv():
     Converts excel file to csv file
     """
 
-    labeled_data = pd.read_excel(f"{CLEAN_DATA_PATH}/users_to_label.xlsx",
-                                 sheet_name="Random Sample")
-    labeled_data.loc[:, "tweets"] = (labeled_data.loc[:, "tweets"]
-                                     .apply(lambda x: clean_tweet(x)))
+    labeled_data = pd.read_excel(
+        f"{CLEAN_DATA_PATH}/users_to_label.xlsx", sheet_name="Random Sample"
+    )
+    labeled_data.loc[:, "tweets"] = labeled_data.loc[:, "tweets"].apply(
+        lambda x: clean_tweet(x)
+    )
 
-    labeled_data.loc[:, "token"] = labeled_data.apply(lambda x:
-                                                      create_token(x.description,
-                                                                   x.tweets),
-                                                      axis=1)
+    labeled_data.loc[:, "token"] = labeled_data.apply(
+        lambda x: create_token(x.description, x.tweets), axis=1
+    )
 
-    labeled_data.to_csv(f"{CLEAN_DATA_PATH}/users_to_label.csv", index=False)
+    # labeled_data.to_csv(f"{CLEAN_DATA_PATH}/users_to_label.csv", index=False)
     return labeled_data
 
 
@@ -71,8 +73,9 @@ def preprocess_function(examples):
     Preprocesses labeled data with HF and Transformers
     library
     """
-    preprocessed_data = tokenizer(examples['token'], truncation=True,
-                                  padding='max_length')
+    preprocessed_data = tokenizer(
+        examples["token"], truncation=True, padding="max_length"
+    )
 
     return preprocessed_data
 
