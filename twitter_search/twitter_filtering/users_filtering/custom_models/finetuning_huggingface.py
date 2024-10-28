78e23c6816bfda51bb64daa3b4fbd14a7fc2900e
Federico Domínguez Molina <fd.molina@outlook.com>
2024-10-28 11:59:22 -0500

    Begin refactoring into class

--- twitter_search/twitter_filtering/users_filtering/custom_models/finetuning_huggingface.py
+++ twitter_search/twitter_filtering/users_filtering/custom_models/finetuning_huggingface.py
@@ -4,100 +4,108 @@ Adding script to finetune the HF NLP model
 from pathlib import Path
 
 import pandas as pd
-from transformers import BartTokenizer, BartForSequenceClassification, Trainer, TrainingArguments
+from transformers import (
+    BartTokenizer,
+    BartForSequenceClassification,
+    Trainer,
+    TrainingArguments,
+)
 
 
+class ModelFinetuner:
+    HUGGINGFACE_PIPELINE = "zero-shot-classification"
+    HUGGINGFACE_MODEL = "facebook/bart-large-mnli"
+    RELEVANT_LABELS = [
+        "environment or pollution",
+        "environmental research",
+        "politician or policymaker",
+        "nonprofit organization",
+        "news outlet or journalist",
+        "other",
+    ]
+    UNDESIRED_CHARS = ["[", "]", "\n", "  "]
 
-script_path = Path(__file__).resolve()
-project_root = script_path.parents[3]
-CLEAN_DATA_PATH = project_root / "data" / "labeled_data"
+    script_path = Path(__file__).resolve()
+    project_root = script_path.parents[3]
+    CLEAN_DATA_PATH = project_root / "data" / "labeled_data"
 
-HUGGINGFACE_PIPELINE = "zero-shot-classification"
-HUGGINGFACE_MODEL = "facebook/bart-large-mnli"
-RELEVANT_LABELS = [
-    "environment or pollution",
-    "environmental research",
-    "politician or policymaker",
-    "nonprofit organization",
-    "news outlet or journalist",
-    "other",
-]
+    def __init__(self):
+        # Load tokenizer and pre-trained model
+        self.tokenizer = BartTokenizer.from_pretrained(self.HUGGINGFACE_MODEL)
+        self.model = BartForSequenceClassification.from_pretrained(
+            self.HUGGINGFACE_MODEL, num_labels=len(self.RELEVANT_LABELS)
+        )
 
+    def clean_tweet(self, tweet):
+        """
+        Cleans a given tweet
+        """
 
-UNDESIRED_CHARS = ["[", "]", "\n", "  "]
+        for undesired_char in self.UNDESIRED_CHARS:
+            tweet = tweet.replace(undesired_char, "")
 
-# Load tokenizer and pre-trained model
-tokenizer = BartTokenizer.from_pretrained(HUGGINGFACE_MODEL)
-model = BartForSequenceClassification.from_pretrained(HUGGINGFACE_MODEL,
-                                                      num_labels=len(RELEVANT_LABELS))
+        return tweet
 
-
-def clean_tweet(tweet):
-    """
-    Cleans a given tweet
-    """
-
-    for undesired_char in UNDESIRED_CHARS:
-        tweet = tweet.replace(undesired_char, "")
-
-    return tweet
-
-
-def create_token(description, tweets):
-    """
-    Creates a token by joining descriptions
-    and tweets
-    """
-    if tweets:
-        token = f"{description} {tweets}"
-    else:
-        if description:
-            token = str(description)
+    @staticmethod
+    def create_token(description, tweets):
+        """
+        Creates a token by joining descriptions
+        and tweets
+        """
+        if tweets:
+            token = f"{description} {tweets}"
         else:
-            token = ""
-    return token
+            if description:
+                token = str(description)
+            else:
+                token = ""
+        return token
 
+    def parsing_labeled_data(self):
+        """
+        Parses the excel file for posterior HF preprocessing
+        """
 
-def parsing_labeled_data():
-    """
-    Converts excel file to csv file
-    """
+        labeled_data = pd.read_excel(
+            f"{self.CLEAN_DATA_PATH}/users_to_label.xlsx", sheet_name="Random Sample"
+        )
+        labeled_data.loc[:, "tweets"] = labeled_data.loc[:, "tweets"].apply(
+            lambda x: self.clean_tweet(x)
+        )
 
-    labeled_data = pd.read_excel(
-        f"{CLEAN_DATA_PATH}/users_to_label.xlsx", sheet_name="Random Sample"
-    )
-    labeled_data.loc[:, "tweets"] = labeled_data.loc[:, "tweets"].apply(
-        lambda x: clean_tweet(x)
-    )
+        labeled_data.loc[:, "token"] = labeled_data.apply(
+            lambda x: self.create_token(x.description, x.tweets), axis=1
+        )
 
-    labeled_data.loc[:, "token"] = labeled_data.apply(
-        lambda x: create_token(x.description, x.tweets), axis=1
-    )
+        return labeled_data
 
-    return labeled_data
+    def preprocess_function(self, token):
+        """
+        Preprocesses labeled data with HF and Transformers
+        library
+        """
+        preprocessed_data = self.tokenizer(
+            token, truncation=True, padding="max_length", max_length=512
+        )
 
+        return preprocessed_data
 
-def preprocess_function(token):
-    """
-    Preprocesses labeled data with HF and Transformers
-    library
-    """
-    preprocessed_data = tokenizer(token, truncation=True,
-                                  padding="max_length", max_length=512)
+    def preprocess_labeled_data(self):
+        """
+        Applies the entire pre-processing steps
+        to the labeled data for finetuning
+        """
+        labeled_data = self.parsing_labeled_data()
+        # Apply the tokenizer to each 'token' entry and store the tokenized data
+        labeled_data["tokenized"] = labeled_data["token"].apply(
+            lambda x: self.preprocess_function(x)
+        )
 
-    return preprocessed_data
+        # Expand 'tokenized' column into separate columns for
+        # 'input_ids' and 'attention_mask'
+        tokenized_columns = labeled_data["tokenized"].apply(pd.Series)
+        labeled_data = pd.concat([labeled_data, tokenized_columns], axis=1)
 
-
-labeled_data = parsing_labeled_data()
-# Apply the tokenizer to each 'token' entry and store the tokenized data
-labeled_data["tokenized"] = labeled_data["token"].apply(lambda x:
-                                                        preprocess_function(x))
-
-# Expand 'tokenized' column into separate columns for 'input_ids' and 'attention_mask'
-tokenized_columns = labeled_data["tokenized"].apply(pd.Series)
-labeled_data = pd.concat([labeled_data, tokenized_columns], axis=1)
-
-# Drop the original 'tokenized' column if no longer needed
-labeled_data.drop(columns=["tokenized"], inplace=True)
-print(labeled_data[["tokenized",
-                    "input_ids", "attention_mask"]].head())
+        # Drop the original 'tokenized' column
+        labeled_data.drop(columns=["tokenized"], inplace=True)
+        print(labeled_data[["tokenized", "input_ids", "attention_mask"]].head())
