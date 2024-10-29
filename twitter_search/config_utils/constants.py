c0592cecb8cd8f9d5cf693761f2ab2faabfab7ee
Federico Domínguez Molina <fd.molina@outlook.com>
2024-10-28 19:11:20 -0500

    Removing index_to_ignore list

--- twitter_search/config_utils/constants.py
+++ twitter_search/config_utils/constants.py
@@ -449,37 +449,6 @@ CATEGORIES = {
     "Government": GOVERNMENT_KEYWORDS,
 }
 
-# words to be ignored in indexing
-INDEX_IGNORE = (
-    "a",
-    "an",
-    "and",
-    "&",
-    "are",
-    "as",
-    "at",
-    "be",
-    "by",
-    "for",
-    "from",
-    "has",
-    "he",
-    "in",
-    "is",
-    "it",
-    "its",
-    "of",
-    "on",
-    "that",
-    "the",
-    "to",
-    "was",
-    "were",
-    "will",
-    "with",
-)
-
-
 dict = {
     "TV Shows": 3,
     "TV Episodes": 4,
