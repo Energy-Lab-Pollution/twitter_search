1e0b4d077c0cc95a462294897f039296f908a475
Federico Domínguez Molina <fd.molina@outlook.com>
2024-11-06 12:28:28 -0600

    fixing queries in spanish

--- twitter_search/config_utils/queries.py
+++ twitter_search/config_utils/queries.py
@@ -25,8 +25,8 @@ QUERIES = {
 
 QUERIES_ES = {
     "media": """location (media OR prensa OR periodico OR transmision
-                OR breaking news OR periodismo OR periodista
-                OR local OR noticias) -is:retweet""",
+                          OR periodismo OR periodista
+                          OR local OR noticias) -is:retweet""",
     "organizations": """((NGO location) OR (organizacion location)
                        OR (non-profit location) OR (location institucion) OR
                        (non-governmental organization location) OR (nonprofit location))
@@ -35,7 +35,8 @@ QUERIES_ES = {
                     OR magistrado OR diputado OR senador OR cabinete OR (gobierno local)
                     OR (MLA) OR (MP) OR gobierno OR municipio OR delegacion)
                     -is:retweet""",
-    "politicians": """location (politica OR politicos OR politico OR politicas) -is:retweet""",
+    "politicians": """location (politica OR politicos OR politico OR politicas)
+                      -is:retweet""",
     "researcher": """location ((investigacion ambiental) OR (investigador ambiental)
                     OR ciencia OR academico OR investigacion OR universidad OR profesor
                     OR postdoc OR postdoctoral OR PhD OR doctorado OR investigador
@@ -43,5 +44,5 @@ QUERIES_ES = {
                     -is:retweet""",
     "environment": """location ((contaminacion del aire) OR contaminacion OR
                     (salud publica) OR (poor air) OR asma OR contaminado OR contaminada
-                    OR smog OR (calidad del aire)) -is:retweet""",
+                    OR smog OR (calidad del aire) OR (medio ambiente)) -is:retweet""",
 }
