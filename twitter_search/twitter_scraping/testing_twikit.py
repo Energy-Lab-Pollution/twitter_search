7a042c9a9f31eaa615e66a0f8d6997c762de63db
Federico Domínguez Molina <fd.molina@outlook.com>
2024-11-26 09:51:11 -0600

    Linting

--- twitter_search/twitter_scraping/testing_twikit.py
+++ twitter_search/twitter_scraping/testing_twikit.py
@@ -1,29 +1,29 @@
-# Enter your account information
-import asyncio
+"""
+Twikit testing script
+"""
 
+# import asyncio
+
+from config_utils.config import EMAIL, PASSWORD, USERNAME
 from twikit import Client
 
 
-USERNAME = ""
-EMAIL = ""
-PASSWORD = ""
+client = Client("en-US")
 
-client = Client('en-US')
 
 async def main():
     # Asynchronous client methods are coroutines and
     # must be called using `await`.
     await client.login(
-        auth_info_1=USERNAME,
-        auth_info_2=EMAIL,
-        password=PASSWORD
+        auth_info_1=USERNAME, auth_info_2=EMAIL, password=PASSWORD
     )
 
     ###########################################
 
     # Search Latest Tweets
-    tweets = await client.search_tweet('query', 'Latest')
+    tweets = await client.search_tweet("query", "Latest")
     for tweet in tweets:
         print(tweet)
-    # Search more tweets
-    more_tweets = await tweets.next()
+
+    # # Search more tweets
+    # more_tweets = await tweets.next()
