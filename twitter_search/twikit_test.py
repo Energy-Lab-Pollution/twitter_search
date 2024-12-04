e280518b553b31cb60f82986cfd3bfeac430f11a
Federico Domínguez Molina <fd.molina@outlook.com>
2024-12-04 15:02:29 -0600

    Update twikit, twitter extraction now works -- updated dependencies

--- twitter_search/twikit_test.py
+++ twitter_search/twikit_test.py
@@ -4,7 +4,7 @@ Twikit testing script
 
 # import asyncio
 
-from twitter_search.config_utils.config import EMAIL, PASSWORD, USERNAME
+import asyncio
 from twikit import Client
 
 
@@ -14,16 +14,21 @@ client = Client("en-US")
 async def main():
     # Asynchronous client methods are coroutines and
     # must be called using `await`.
-    await client.login(
-        auth_info_1=USERNAME, auth_info_2=EMAIL, password=PASSWORD
-    )
+    # await client.login(
+    #     auth_info_1=USERNAME, auth_info_2=EMAIL, password=PASSWORD
+    # )
+
+    client.load_cookies("twitter_search/config_utils/cookies.json")
 
     ###########################################
 
     # Search Latest Tweets
     tweets = await client.search_tweet("New York AND Pollution", "Latest")
     for tweet in tweets:
-        print(tweet)
+        print(tweet.text)
 
     # # Search more tweets
     # more_tweets = await tweets.next()
+
+if __name__ == "__main__":
+    asyncio.run(main())
