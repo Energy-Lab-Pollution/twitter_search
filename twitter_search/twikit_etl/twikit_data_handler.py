2d7e16c0e17714048fe1e652f0089aebc83e916e
Federico Domínguez Molina <fd.molina@outlook.com>
2024-12-06 18:06:35 -0600

    Begin building Twikit class

---
+++ twitter_search/twikit_etl/twikit_data_handler.py
@@ -0,0 +1,79 @@
+"""
+Script that runs the Twitter Search Pipeline by using Twikit
+"""
+
+from pathlib import Path
+
+from config_utils.cities import ALIAS_DICT, CITIES, PILOT_CITIES
+from config_utils.queries import QUERIES
+
+
+class TwikitDataHandler:
+    """
+    Class that handles the Twikit search and data collection process
+    """
+    QUERIES = QUERIES
+    CITIES = CITIES
+
+    def __init__(self, location, account_type, num_iterations=1):
+        self.location = location.lower()
+        self.account_type = account_type
+        self.num_iterations = num_iterations
+        self.base_dir = Path(__file__).parent.parent / "data/raw_data"
+
+    def run(self):
+        """
+        Runs the entire process
+        """
+        for count in range(1, self.num_iterations + 1):
+            self.process_iteration(count)
+            if count == self.num_iterations:
+                break
+
+    def setup_file_paths(self, count):
+        """
+        Set up file paths for the current iteration.
+
+        Args:
+            count (int): The current iteration count.
+
+        We check if the current location is an alias of a main city
+        and set the files accordingly
+        """
+
+        print("Checking if city is in secondary cities dictionary")
+        if self.location in ALIAS_DICT:
+            print(f"{self.location} found in alias dict")
+            file_city = ALIAS_DICT[self.location]
+        else:
+            file_city = self.location
+
+        self.paths = {
+            "output_file_users": self.base_dir
+            / f"{file_city}_{self.account_type}_users_test.json",
+            "output_file_tweets": self.base_dir
+            / f"{file_city}_{self.account_type}_tweets_test.json",
+            "output_file_processing": self.base_dir
+            / f"{file_city}_{self.account_type}_processed_users.json",
+            "output_file_filter": self.base_dir
+            / f"{file_city}_{self.account_type}_users_filtered_{count}.json",
+            "output_file_tweet_add": self.base_dir
+            / f"{file_city}_{self.account_type}_users_tweet_added",
+        }
+
+        input_file_processing = (
+            self.paths["output_file_tweets"],
+            self.paths["output_file_users"],
+        )
+
+        self.paths["input_file_processing"] = input_file_processing
+
+        if count == 1:
+            output_file_processing = self.paths["output_file_processing"]
+            self.paths["input_file_filter"] = output_file_processing
+
+        else:
+            self.paths["input_file_filter"] = (
+                self.base_dir
+                / f"{file_city}_{self.account_type}_totalusers_{count - 1}.json"
+            )
