85380d04fb170e88504a9ccd313bbb893b2bcb98
Federico Domínguez Molina <fd.molina@outlook.com>
2024-12-20 14:16:37 -0600

    Adding function to get tweets and users

--- twitter_search/twikit_etl/data_collection/search_twikit_users.py
+++ twitter_search/twikit_etl/data_collection/search_twikit_users.py
@@ -11,6 +11,36 @@ from twikit import Client
 client = Client("en-US")
 
 
+def parse_tweets_and_users(tweets):
+    """
+    Parses tweets and users from twikit
+    """
+    tweets_list = []
+    users_list = []
+    for tweet in tweets:
+        tweet_dict = {}
+        tweet_dict["tweet_id"] = tweet.id
+        tweet_dict["text"] = tweet.text
+        tweet_dict["created_at"] = tweet.created_at
+        tweet_dict["author_id"] = tweet.user.id
+
+        user_dict = {}
+        user_dict["user_id"] = tweet.user.id
+        user_dict["username"] = tweet.user.name
+        user_dict["description"] = tweet.user.description
+        user_dict["location"] = tweet.user.location
+        user_dict["name"] = tweet.user.screen_name
+        user_dict["url"] = tweet.user.url
+        user_dict["tweets"] = [tweet.text]
+        user_dict["geo_code"] = []
+
+        tweets_list.append(tweet_dict)
+        users_list.append(user_dict)
+
+        return tweets_list, users_list
+
+
+
 async def main():
     # Asynchronous client methods are coroutines and
     # must be called using `await`.
@@ -19,14 +49,6 @@ async def main():
 
     # Search Latest Tweets
     tweets = await client.search_tweet("New York AND Pollution", "Latest", count=100)
-    tweets_list = []
-    for tweet in tweets:
-        tweet_dict = {}
-        tweet_dict["tweet_id"] = tweet.id
-        tweet_dict["text"] = tweet.text
-        tweet_dict["created_at"] = tweet.created_at
-        tweet_dict["author_id"] = tweet.user.id
-
-        tweets_list.append(tweet_dict)
+    tweets_list, users_list = parse_tweets_and_users(tweets)
 
     print(tweets_list)
