501efe8dfc91674698e9c535381382572c5cb970
Federico Domínguez Molina <fd.molina@outlook.com>
2025-01-06 14:53:25 -0600

    Experimenting with await previous and await next

--- twitter_search/twikit_etl/data_collection/search_twikit_users.py
+++ twitter_search/twikit_etl/data_collection/search_twikit_users.py
@@ -7,9 +7,12 @@ import json
 
 from twikit import Client
 
+QUERY = """location ((air pollution) OR pollution OR (public health)
+                OR (poor air) OR asthma OR polluted OR (pollution control board)
+                OR smog OR (air quality)) -is:retweet"""
+
 
 client = Client("en-US")
-
 
 def parse_tweets_and_users(tweets):
     """
@@ -45,10 +48,26 @@ async def main():
     # Asynchronous client methods are coroutines and
     # must be called using `await`.
 
-    client.load_cookies("twitter_search/config_utils/cookies.json")
+    client.load_cookies("../../twitter_search/config_utils/cookies.json")
 
     # Search Latest Tweets
-    tweets = await client.search_tweet("New York AND Pollution", "Latest", count=100)
+    query = QUERY.replace("location", "New York")
+    tweets = await client.search_tweet(query, "Latest", count=20)
     tweets_list, users_list = parse_tweets_and_users(tweets)
-
     print(tweets_list)
+
+    previous_tweets = await tweets.previous()
+    if previous_tweets:
+        previous_tweets_list, previous_users_list = parse_tweets_and_users(previous_tweets)
+
+    next_tweets = await tweets.next()
+    if next_tweets:
+        next_tweets_list, next_users_list = parse_tweets_and_users(previous_tweets)
+        print(next_tweets_list)
+
+
+    
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
