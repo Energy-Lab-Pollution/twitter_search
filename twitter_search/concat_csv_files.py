75ca96658c8ea89b92e4a35c5ff0eb33c12f7d4a
Federico Domínguez Molina <fd.molina@outlook.com>
2025-03-05 09:51:00 -0600

    Removing unused imports

--- twitter_search/concat_csv_files.py
+++ twitter_search/concat_csv_files.py
@@ -1,12 +1,6 @@
 """
 Detects all generated .csv files and concatenates them into a single file
 """
-
-import os
-from pathlib import Path
-
-import pandas as pd
-
 from etl.csv_concat import CSVConcat
 
 if __name__ == "__main__":
