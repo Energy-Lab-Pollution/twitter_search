3fd4bd89120addd090237096f7864d488cea68cc
Federico Domínguez Molina <fd.molina@outlook.com>
2025-03-14 13:10:30 -0500

    Adding (near: location) statement to search queries

--- twitter_search/config_utils/queries.py
+++ twitter_search/config_utils/queries.py
@@ -5,24 +5,24 @@ Dictionary with the current queries for searching users
 QUERIES = {
     "media": """location (media OR press OR coverage OR broadcasting
                 OR (breaking news) OR journalism OR journalist
-                OR local OR news) -is:retweet""",
+                OR local OR news) (near: location) -is:retweet""",
     "organizations": """((NGO location) OR (organization location)
                        OR (non-profit location) OR (location institution) OR
                        (non-governmental organization location) OR (nonprofit location))
-                       -is:retweet""",
+                       (near: location) (near: location) -is:retweet""",
     "policymaker": """location ((member of parliament) OR governor OR minister
                     OR magistrate OR (district magistrate) OR IAS OR officer
                     OR cabinet OR mayor OR councillor OR (local government)
                     OR (city official) OR (MLA) OR (MP) OR government OR municipality)
-                    -is:retweet""",
+                    (near: location) -is:retweet""",
     "politicians": """location (politics OR politicians OR politician) -is:retweet""",
     "researcher": """location ((environmental research) OR (environmental researcher)
                     OR science OR academic OR research OR university OR professor OR
-                    postdoc OR postdoctoral OR PhD)
+                    postdoc OR postdoctoral OR PhD) (near: location)
                     -is:retweet""",
     "environment": """location ((air pollution) OR pollution OR (public health)
                 OR (poor air) OR asthma OR polluted OR (pollution control board)
-                OR smog OR (air quality)) -is:retweet""",
+                OR smog OR (air quality)) (near: location) -is:retweet""",
 }
 
 
