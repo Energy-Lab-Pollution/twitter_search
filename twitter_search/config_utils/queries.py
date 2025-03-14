125f2822294be6d9ad86470c2313b1f1c9456acd
Federico Domínguez Molina <fd.molina@outlook.com>
2025-03-14 13:28:41 -0500

    Adding (near: location) statement to search queries

--- twitter_search/config_utils/queries.py
+++ twitter_search/config_utils/queries.py
@@ -29,50 +29,54 @@ QUERIES = {
 QUERIES_ES = {
     "media": """location (media OR prensa OR periodico OR transmision
                           OR periodismo OR periodista
-                          OR local OR noticias) -is:retweet""",
+                          OR local OR noticias) (near: location) -is:retweet""",
     "organizations": """location ("NGO" OR "organizacion" OR "organizaciones" OR
                      "organizacion civil" OR "non-profit" OR "institucion" OR
                      "sin fines de lucro" OR "non-governmental organization" OR
-                     "nonprofit") -is:retweet""",
+                     "nonprofit") (near: location) -is:retweet""",
     "policymaker": """location (gobernador OR ministro OR magistrado OR diputado
                     OR senador OR gabinete OR (gobierno local) OR gobierno OR
                     municipio OR delegacion OR ministerio OR legislador OR legisladores
-                    OR legislacion OR legisladora)
+                    OR legislacion OR legisladora) (near: location)
                     -is:retweet""",
-    "politicians": """location (politica OR politicos OR politico OR politicas)
+    "politicians": """location (politica OR politicos OR politico OR politicas) (near: location)
                       -is:retweet""",
     "researcher": """location ((investigacion ambiental) OR (investigador ambiental)
                     OR (investigadora ambiental) OR ciencia OR academico
                     OR academica OR investigacion OR universidad OR profesor
                     OR postdoc OR postdoctoral OR PhD OR doctorado OR investigador
-                    OR investigadora OR universitario OR universitaria) -is:retweet""",
+                    OR investigadora OR universitario OR universitaria) (near: location) 
+                    -is:retweet""",
     "environment": """location ((contaminacion del aire) OR contaminacion OR
                     (salud publica) OR (poor air) OR asma OR contaminado OR contaminada
-                    OR smog OR (calidad del aire) OR (medio ambiente)) -is:retweet""",
+                    OR smog OR (calidad del aire) OR (medio ambiente)) (near: location)
+                    -is:retweet""",
 }
 
 
 QUERIES_FR = {
     "media": """location (média OR presse OR couverture OR diffusion
                 OR (flash info) OR journalisme OR journaliste
-                OR local OR actualités) -is:retweet""",
+                OR local OR actualités) (near: location) 
+                -is:retweet""",
     "organizations": """((ONG location) OR (organisation location)
                        OR (non lucratif location) OR (location institution) OR
                        (organisation non gouvernementale location) OR (non lucratif location))
-                       -is:retweet""",
+                       (near: location) -is:retweet""",
     "policymaker": """location ((membre du parlement) OR gouverneur OR ministre
                     OR magistrat OR (magistrat de district) OR IAS OR officier
                     OR cabinet OR maire OR conseiller municipal OR (gouvernement local)
                     OR (fonctionnaire municipal) OR (MLA) OR (député) OR gouvernement OR municipalité)
-                    -is:retweet""",
-    "politicians": """location (politique OR politiciens OR politicien) -is:retweet""",
+                    (near: location) -is:retweet""",
+    "politicians": """location (politique OR politiciens OR politicien) (near: location)
+                      -is:retweet""",
     "researcher": """location ((recherche environnementale) OR (chercheur environnemental)
                     OR science OR universitaire OR recherche OR université OR professeur OR
-                    postdoc OR postdoctoral OR doctorat)
+                    postdoc OR postdoctoral OR doctorat) (near: location)
                     -is:retweet""",
     "environment": """location ((pollution de l'air) OR pollution OR (santé publique)
                 OR (air de mauvaise qualité) OR asthme OR pollué OR (commission de contrôle de la pollution)
-                OR smog OR (qualité de l'air)) -is:retweet""",
+                OR smog OR (qualité de l'air)) (near: location) -is:retweet""",
 }
 
 
