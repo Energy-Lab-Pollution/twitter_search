4d8f558b41391828311c751c72009f57f08de502
Federico Domínguez Molina <fd.molina@outlook.com>
2025-03-20 13:38:55 -0500

    Adding function to parse retweeters

--- twitter_search/network/user_network.py
+++ twitter_search/network/user_network.py
@@ -12,7 +12,7 @@ user_id = "1652537276"
 client = twikit.Client("en-US")
 client.load_cookies(TWIKIT_COOKIES_DIR)
 
-async def get_users_tweets(client):
+async def get_users_tweets(client, user_id):
     """
     Get a user by id and then get his tweets
 
@@ -23,9 +23,27 @@ async def get_users_tweets(client):
     Returns:
         - user_tweets: 
     """
-    user_tweets = await client.get_user_tweets()
+    user_tweets = await client.get_user_tweets(user_id)
     
     return user_tweets
+
+def parse_retweeters(retweeters):
+    """
+    Parse retweeters (user objects) and
+    put them into a list of dictionaries
+    """
+    retweeters_list = []
+
+    if retweeters:
+        for retweeter in retweeters:
+
+            retweeter_dict = {}
+            retweeter_dict["user_id"] = retweeter.id
+            retweeter_dict["username"] = retweeter.screen_name
+
+            retweeters_list.append(retweeter_dict)
+
+    return retweeters_list
 
 
 def get_users_retweeters(tweets):
@@ -42,12 +60,16 @@ def get_users_retweeters(tweets):
         tweet_id = tweet.id 
         retweeters = tweet.get_retweeters()
 
-        tweet_dict[tweet.id] = retweeters
+        tweet_dict[tweet_id] = retweeters
         dict_list.append(tweet_dict)
 
     return dict_list
 
     
+
+if __name__ == "__main__":
+
+    user_tweets = asyncio.run()
 
 
 
