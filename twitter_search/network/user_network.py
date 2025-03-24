d0be35d59584f322adc20d3baefe9959bdbb31e4
Federico Domínguez Molina <fd.molina@outlook.com>
2025-03-24 16:32:46 -0500

    Refactoring code to class

--- twitter_search/network/user_network.py
+++ twitter_search/network/user_network.py
@@ -16,58 +16,76 @@ client = twikit.Client("en-US")
 client.load_cookies(TWIKIT_COOKIES_DIR)
 
 
-async def get_retweeters_followers(client, user_id, extraction_type):
-    """
-    For a given user, we extract their retweeters and followers
 
-    Args
-    -------
-        - client: Twikit client obj
+class UserNetwork:
 
-    Returns:
-        - user_tweets:
-    """
+    def __init__(self):
+        self.client = twikit.Client("en-US")
+        self.client.load_cookies(TWIKIT_COOKIES_DIR)
 
-    dict_list = []
-    
-    if extraction_type == "retweeters":
+
+    @staticmethod
+    def parse_retweeters(retweeters):
+        """
+        Parse retweeters (user objects) and
+        put them into a list of dictionaries
+        """
+        retweeters_list = []
+
+        if retweeters:
+            for retweeter in retweeters:
+                retweeter_dict = {}
+                retweeter_dict["user_id"] = retweeter.id
+                retweeter_dict["username"] = retweeter.screen_name
+                retweeter_dict["description"] = retweeter.description
+                retweeter_dict["location"] = retweeter.location
+                retweeter_dict["followers_count"] = retweeter.followers_count
+                retweeter_dict["following_count"] = retweeter.following_count
+
+                retweeters_list.append(retweeter_dict)
+
+        return retweeters_list
+
+
+    async def get_retweeters(self, client, user_id):
+        """
+        For a given user, we extract their retweeters and followers
+
+        Args
+        -------
+            - client: Twikit client obj
+
+        Returns:
+            - user_tweets:
+        """
+
+        dict_list = []
+        
         user_tweets = await client.get_user_tweets(user_id, "Tweets")
         time.sleep(2)
-        for tweet in user_tweets[:10]:
+        for tweet in user_tweets:
             tweet_dict = {}
             tweet_id = tweet.id
             retweeters = await tweet.get_retweeters()
-            retweeters = parse_retweeters(retweeters)
+            retweeters = self.parse_retweeters(retweeters)
 
             tweet_dict[tweet_id] = retweeters
             dict_list.append(tweet_dict)
 
-        user = await client.get_user_by_id(user_id)
-        user_followers = await user.get_followers()
+        return dict_list
+
+    async def get_followers(self, client, user_id):
+        """
+        Gets a given user's followers
+        """
+        
+        user_followers = await client.get_followers(user_id)
 
         if user_followers:
             pass
 
-    return dict_list
-
-def parse_retweeters(retweeters):
-    """
-    Parse retweeters (user objects) and
-    put them into a list of dictionaries
-    """
-    retweeters_list = []
-
-    if retweeters:
-        for retweeter in retweeters:
-            retweeter_dict = {}
-            retweeter_dict["user_id"] = retweeter.id
-            retweeter_dict["username"] = retweeter.screen_name
-
-            retweeters_list.append(retweeter_dict)
-
-    return retweeters_list
-
 
 if __name__ == "__main__":
-    user_retweeters = asyncio.run(get_user_retweeters(client, user_id))
-    print(user_retweeters)
+    pass
+    # user_retweeters = asyncio.run(get_retweeters_followers(client, user_id))
+    # print(user_retweeters)
