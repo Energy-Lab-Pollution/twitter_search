5786bc621861076073325610ebb551f79de388ae
Federico Domínguez Molina <fd.molina@outlook.com>
2025-03-25 15:35:32 -0500

    Importing Twikit constants, trying to refactor method to get retweeters from a particular user

--- twitter_search/network/user_network.py
+++ twitter_search/network/user_network.py
@@ -3,20 +3,22 @@ Script to pull tweets and retweeters from a particular user,
 """
 import asyncio
 import time
-
 import twikit
+
+from config_utils.constants import TWIKIT_COOKIES_DIR, TWIKIT_THRESHOLD
 
 
 # from config_utils.constants import TWIKIT_COOKIES_DIR
 user_id = "1652537276"
-TWIKIT_COOKIES_DIR = "twitter_search/config_utils/cookies.json"
+# TWIKIT_COOKIES_DIR = "twitter_search/config_utils/cookies.json"
 
 
 class UserNetwork:
+    TWIKIT_THRESHOLD = TWIKIT_THRESHOLD
+    TWIKIT_COOKIES_DIR = TWIKIT_COOKIES_DIR
     def __init__(self):
         self.client = twikit.Client("en-US")
-        self.client.load_cookies(TWIKIT_COOKIES_DIR)
-
+        self.client.load_cookies(self.TWIKIT_COOKIES_DIR)
     @staticmethod
     def parse_users(users):
         """
@@ -39,9 +41,33 @@ class UserNetwork:
 
         return users_list
 
-    async def get_retweeters(self, client, user_id):
+    async def get_tweets_retweeters(self, tweets):
         """
-        For a given user, we extract their retweeters and followers
+        Given a set of tweets, we get a list of dictionaries
+        with the twees' and retweeters' information
+        
+        Args:
+            - tweets
+        """
+        dict_list = []
+        if tweets:
+            for tweet in tweets:
+                tweet_dict = {}
+                tweet_id = tweet.id
+                retweeters = await tweet.get_retweeters()
+                retweeters = self.parse_users(retweeters)
+
+                tweet_dict[tweet_id] = retweeters
+                dict_list.append(tweet_dict)
+
+        return dict_list
+
+
+    async def get_user_retweeters(self, client, user_id):
+        """
+        For a given user, we extract the users who have retweeted them
+        in the past. We get the largest number of possible tweets and, for
+        each tweet, we get the retweeters.
 
         Args
         -------
@@ -50,19 +76,31 @@ class UserNetwork:
         Returns:
             - user_tweets:
         """
-
+        # We need to get tweets first 
         dict_list = []
+        more_tweets_available = True
+        num_iter = 1
 
         user_tweets = await client.get_user_tweets(user_id, "Tweets")
-        for tweet in user_tweets:
-            tweet_dict = {}
-            tweet_id = tweet.id
-            retweeters = await tweet.get_retweeters()
-            retweeters = self.parse_retweeters(retweeters)
+        tweets_list = asyncio.run(self.get_user_retweeters)
+        dict_list.extend(tweets_list)
 
-            tweet_dict[tweet_id] = retweeters
-            dict_list.append(tweet_dict)
 
+        while more_tweets_available:
+            next_tweets = await user_tweets.next()
+            if next_tweets:
+                tweets_list = asyncio.run(self.get_user_retweeters)
+                dict_list.extend(tweets_list)
+
+            else:
+                more_tweets_available = False
+
+            if num_iter % 5 == 0:
+                print(f"Processed {num_iter} batches")
+
+            if num_iter == self.TWIKIT_THRESHOLD:
+                break
+        
         return dict_list
 
     async def get_followers(self, client, user_id):
