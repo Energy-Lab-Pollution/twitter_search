60be80321be775e1ed006704ff0a143329b995b7
Federico Domínguez Molina <fd.molina@outlook.com>
2025-03-28 19:42:12 -0500

    Added argparser facilities

--- twitter_search/run_network.py
+++ twitter_search/run_network.py
@@ -2,9 +2,23 @@
 Script to get followers and retweeters from a particular set
 of X Users
 """
+from argparse import ArgumentParser
+import time
 import asyncio
+from config_utils.constants import FIFTEEN_MINUTES
 from network.network_handler import NetworkHandler
 
 if __name__ == "__main__":
-    network_handler = NetworkHandler("kolkata", 5)
+    parser = ArgumentParser("Parameters to get users data to generate a network")
+    parser.add_argument("location", type=str, help="Location to read users from")
+    parser.add_argument("num_users", type=int, help="Number of users to process")
+    parser.add_argument("wait", type=str, choices=["Yes", "No"], help="Decide whether to wait 15 mins or not")
+
+    args = parser.parse_args()
+    
+    if args.wait == "Yes":
+        print("Sleeping for 15 minutes...")
+        time.sleep(FIFTEEN_MINUTES)
+
+    network_handler = NetworkHandler(args.location, args.num_users)
     asyncio.run(network_handler.run())
