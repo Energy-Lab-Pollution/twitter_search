b4919d912d9bfe29dc9956e02e6c76f0d7a803b2
Federico Domínguez Molina <fd.molina@outlook.com>
2025-04-10 11:09:52 -0500

    Adding methods to get users via tweeket

--- twitter_search/network/network_handler.py
+++ twitter_search/network/network_handler.py
@@ -11,8 +11,8 @@ import time
 from pathlib import Path
 
 import pandas as pd
-from config_utils.cities import ALIAS_DICT
-from config_utils.constants import FIFTEEN_MINUTES
+from config_utils.cities import CITIES, ALIAS_DICT
+from config_utils.constants import FIFTEEN_MINUTES, TWIKIT_COUNT
 from network.user_network import UserNetwork
 
 
@@ -38,11 +38,95 @@ class NetworkHandler:
             self.base_dir / f"networks/{self.location}/{self.location}.json"
         )
 
-    def search_users(self):
+
+    def parse_tweets_and_users(self, tweets):
+            """
+            Parses tweets and users from twikit into dictionaries
+
+            Args:
+                tweets: Array of twikit.tweet.Tweet objects
+
+            Returns:
+                tweets_list: Array of dictionaries with tweets' data
+                users_list: Array of dictionaries with users' data
+            """
+            tweets_list = []
+            users_list = []
+            for tweet in tweets:
+                tweet_dict = {}
+                tweet_dict["tweet_id"] = tweet.id
+                tweet_dict["text"] = tweet.text
+                tweet_dict["created_at"] = tweet.created_at
+                tweet_dict["author_id"] = tweet.user.id
+
+                parsed_date = self.convert_to_yyyy_mm_dd(tweet.created_at)
+
+                user_dict = {}
+                user_dict["user_id"] = tweet.user.id
+                user_dict["username"] = tweet.user.name
+                user_dict["description"] = tweet.user.description
+                user_dict["location"] = tweet.user.location
+                user_dict["name"] = tweet.user.screen_name
+                user_dict["url"] = tweet.user.url
+                user_dict["followers_count"] = tweet.user.followers_count
+                user_dict["following_count"] = tweet.user.following_count
+                user_dict["listed_count"] = tweet.user.listed_count
+                user_dict["tweets"] = [tweet.text]
+                user_dict["tweet_date"] = parsed_date
+                user_dict["user_date_id"] = f"{tweet.user.id}-{parsed_date}"
+                user_dict["geo_code"] = []
+
+                tweets_list.append(tweet_dict)
+                users_list.append(user_dict)
+
+            return tweets_list, users_list
+
+    async def search_tweets_and_users(self):
         """
-        Looks for twikit users
+        Method used to search for tweets, with twikit
+        with the given query
+
+        This method uses twikit's "await next" function
+        to get more tweets with the given query.
         """
-        pass
+
+        tweets = await self.client.search_tweet(
+            self.location, "Latest", count=TWIKIT_COUNT
+        )
+        self.tweets_list, self.users_list = self.parse_tweets_and_users(tweets)
+
+        more_tweets_available = True
+        num_iter = 1
+
+        next_tweets = await tweets.next()
+        if next_tweets:
+            next_tweets_list, next_users_list = self.parse_tweets_and_users(
+                next_tweets
+            )
+            self.tweets_list.extend(next_tweets_list)
+            self.users_list.extend(next_users_list)
+        else:
+            more_tweets_available = False
+
+        while more_tweets_available:
+            next_tweets = await next_tweets.next()
+            if next_tweets:
+                next_tweets_list, next_users_list = self.parse_tweets_and_users(
+                    next_tweets
+                )
+                self.tweets_list.extend(next_tweets_list)
+                self.users_list.extend(next_users_list)
+
+            else:
+                more_tweets_available = False
+
+            if num_iter % 5 == 0:
+                print(f"Processed {num_iter} batches")
+
+            if num_iter == self.twikit_threshold:
+                break
+
+            num_iter += 1
 
     
     def _get_city_users(self):
