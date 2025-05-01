348a535e498e7ef46c1ffa769b7941a483975589
Federico Domínguez Molina <fd.molina@outlook.com>
2025-05-01 16:28:16 -0500

    Adding new main script to get user attribues

---
+++ twitter_search/add_user_attributes.py
@@ -0,0 +1,37 @@
+"""
+Script to get followers and retweeters from a particular set
+of X Users
+"""
+
+import asyncio
+import time
+from argparse import ArgumentParser
+
+from config_utils.constants import FIFTEEN_MINUTES
+from network.user_attributes import UserAttributes
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        "Parameters to get users data to generate a network"
+    )
+    parser.add_argument(
+        "location", type=str, help="Location to read users from"
+    )
+    parser.add_argument(
+        "wait",
+        type=str,
+        choices=["Yes", "No"],
+        help="Decide whether to wait 15 mins or not",
+    )
+
+    args = parser.parse_args()
+
+    if args.wait == "Yes":
+        print("Sleeping for 15 minutes...")
+        time.sleep(FIFTEEN_MINUTES)
+
+    user_attributes = UserAttributes(args.location)
+    asyncio.run(
+        user_attributes.run()
+    )
