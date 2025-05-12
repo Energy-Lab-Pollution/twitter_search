4b566d374fbca961862ef5d6d8915742cfea8e27
Federico Domínguez Molina <fd.molina@outlook.com>
2025-05-12 12:13:35 -0500

    Adding main script for SQS city search

---
+++ twitter_search/search_city_users.py
@@ -0,0 +1,58 @@
+"""
+Script to get followers and retweeters from a particular set
+of X Users
+"""
+
+import asyncio
+import time
+from argparse import ArgumentParser
+
+from config_utils.constants import FIFTEEN_MINUTES
+from network.sqs_city_users import CityUsers
+
+
+if __name__ == "__main__":
+    # unpack cli parameters
+    # instantiate CityUsers class (city_user)
+    # parameters: [location, tweet_count, keywords (both hashtags, timeperiod and keywords)]
+    # check extraction type here
+    # call relevant methods on the city user class
+    #TODO: Add dash-dash to avoid order and be more flexible
+
+    parser = ArgumentParser(
+        "Parameters to get users data to generate a network"
+    )
+    parser.add_argument(
+        "location", type=str, help="Location to read users from"
+    )
+    parser.add_argument(
+        "--extraction_type",
+        type=str,
+        choices=["twikit", "x"],
+        help="Choose how to get users",
+    )
+    parser.add_argument(
+        "--wait",
+        type=str,
+        choices=["Yes", "No"],
+        help="Decide whether to wait 15 mins or not",
+    )
+    parser.add_argument(
+        "--file_flag",
+        type=str,
+        choices=["Yes", "No"],
+        help="Determines if root users will be extracted from the .csv file",
+    )
+    parser.add_argument(
+        "--account_number",
+        type=int,
+        help="Account number to use with twikit",
+    )
+    args = parser.parse_args()
+
+    if args.wait == "Yes":
+        print("Sleeping for 15 minutes...")
+        time.sleep(FIFTEEN_MINUTES)
+
+    file_flag = True if args.file_flag == "Yes" else False
+    city_users = CityUsers(args.location)
