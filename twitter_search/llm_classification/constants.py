de0b5c2373c92d4d7a326732513275485330b595
Federico Domínguez Molina <fd.molina@outlook.com>
2025-06-03 14:59:08 -0600

    Finishing prompt

--- twitter_search/llm_classification/constants.py
+++ twitter_search/llm_classification/constants.py
@@ -4,17 +4,23 @@ user belongs to the provided categories. We will provide the user's
 description, along with several of the user's original tweets.
 With this information, please determine to which category the user belongs.
 
+Also, please give more weight to the description while performing the
+classification. The tweets are important, but the description usually 
+reveals what the user does or is.
+
 You should only use the 'other' category if you feel the user
 does not fit into any of the other classifications.
 
-The categories are the following:
+The categories are the following. I will give you a brief guide
+of which 'professions' encompass such classifications, but feel free
+to make your own decision.
 
 - "media": for journalists, media outlets, etc.
 - "organizations": for NGOs, non-profits, institutions, etc.
-- "policymaker": Like regulators, state treasurers,
+- "policymaker": Like regulators, state treasurers, attorney generals, diplomats, etc
 - "politicians": for presidents, mayors, members of cabinet, etc.
 - "researchers": for PhD students, postdocs, researchers (as the name suggests)
-- "healthcare": for doctors, physicians, psychiatrists, surgeons, medical practitioners, etc.S
+- "healthcare": for doctors, physicians, psychiatrists, surgeons, medical practitioners, etc.
 
 The user's description is the following:
 
@@ -26,8 +32,8 @@ user_tweets
 
 Use this JSON schema to return the result:
 
-Score = {'score': float}
-Return: score
+Category = {'category': str}
+Return: category
 """
 
 ROLE=""
