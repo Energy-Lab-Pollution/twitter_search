2af43c359ac45b49f5cef749ee073cc657302b52
Federico Domínguez Molina <fd.molina@outlook.com>
2025-06-03 14:13:21 -0600

    Modifying query

--- twitter_search/llm_classification/constants.py
+++ twitter_search/llm_classification/constants.py
@@ -1,4 +1,22 @@
-PROMPT=""
+PROMPT="""
+You are analyst with the task of determining if a given 
+user belongs to the provided categories. You will provide a
+the category that you feel is most likely for th.
+
+The user's description is the following:
+
+user_description
+
+The given query is:
+
+given_query
+
+Use this JSON schema to return the result:
+
+Score = {'score': float}
+Return: score
+"""
+
 ROLE=""
 OPENAI_MODEL="gpt-4o-mini"
 GEMINI_MODEL="gemini-2.0-flash"
--- twitter_search/llm_classification/gemini_classifier.py
+++ twitter_search/llm_classification/gemini_classifier.py
@@ -19,14 +19,14 @@ class GeminiAgent:
 
     def send_prompt(
         self,
-        news_article,
-        given_query,
+        user_description,
+        user_tweets,
     ):
         """
         Send prompt to Gemini and parse response to DataFrame
         """
-        self.user_content = self.user_content.replace("news_article_str", news_article)
-        self.user_content = self.user_content.replace("given_query", given_query)
+        self.user_content = self.user_content.replace("user_description", user_description)
+        self.user_content = self.user_content.replace("user_tweets", user_tweets)
         print("Sending prompt to Gemini...")
         model = genai.GenerativeModel(self.model)
         response = model.generate_content(self.user_content)
